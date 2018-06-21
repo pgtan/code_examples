a7e8420810f34bc04a963f2f254647dde7bfabba
Plamen Tanovski <pgt@celsius.home>
2018-06-21 12:29:49 +0200

    added Problem 10

---
+++ 10.el
@@ -0,0 +1,32 @@
+;; -*- lexical-binding: t -*-
+;; Project Euler: Problem 10
+;; Sieve of Sundaram
+
+(require 'seq)
+
+(defun pgt/gen-jlist (n i)
+  ""
+  (number-sequence i (floor (/ (- n i) (1+ (* 2 i))))))
+
+(defun pgt/gen-i-j (i j n)
+  ""
+  (let ((num (+ i j (* 2 i j))))
+    (when (<= num n) num)))
+
+(defun pgt/primes (bool-vec)
+  ""
+  (delq nil (seq-map-indexed (lambda (elt idx) (when elt (1- idx))) bool-vec)))
+
+(defun pgt/sieve-sundaram (n)
+  "sieve of sundaram"
+  (let ((is-prime (vconcat [nil nil] (make-bool-vector (1- n) t)))
+	(ilist (number-sequence 1 (floor (/ (1- (sqrt (1+ (* 2 n)))) 2)))))
+    (dolist (inum ilist (+ 2 (apply #'+ (mapcar (lambda (x) (1+ (* 2 x)))
+						(pgt/primes is-prime)))))
+      (let ((jlist (pgt/gen-jlist n inum)))
+	(dolist (jnum jlist)
+	  (let ((noprime (pgt/gen-i-j inum jnum n)))
+	    (when noprime (aset is-prime (1+ noprime) nil))))))))
+
+(print (pgt/sieve-sundaram 1000001))
+
