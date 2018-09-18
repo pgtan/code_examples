0a971486fbc18b374ad6145e5500fafbb224f06f
Plamen Tanovski <pgt@tanovski.de>
2018-09-18 21:57:02 +0200

    Prolog code rewritten

--- e15.pl
+++ e15.pl
@@ -1,29 +1,24 @@
-%%% Project Euler Problem 15
-:- use_module(library(clpfd)).
+% Project Euler 15
+:- use_module(library(tabling)).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% stolen from https://www.metalevel.at/prolog/memoization %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-:- dynamic memo_/1.
-memo(Goal) :-
-        (   memo_(Goal)
-        ->  true
-        ;   once(Goal),
-            assertz(memo_(Goal))
-        ).
+:- table lattice_paths/3.
 
-lattice_paths(1,1,N) :- N #= 0.
 
-lattice_paths(1,_,N) :- N #= 1.
+lattice_paths(1,1,N) :- N is 0.
 
-lattice_paths(_,1,N) :- N #= 1.
+lattice_paths(1,Y,N) :-
+    Y > 1,
+    N is 1.
+
+lattice_paths(X,1,N) :-
+    X > 1,
+    N is 1.
 
 lattice_paths(X,Y,N) :-
-    Y in 1..21,
-    X in 1..21,
+    X > 0,
+    Y > 0,
     X1 is X - 1,
     Y1 is Y - 1,
-    memo(lattice_paths(X1,Y,N1)),
-    memo(lattice_paths(X,Y1,N2)),
+    lattice_paths(X1,Y,N1),
+    lattice_paths(X,Y1,N2),
     N is N1 + N2.
-
